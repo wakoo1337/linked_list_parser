db23b03180483e1046c88a9e23c027acfe2b6829
Дмитрий Толстокоров <wakoo1337@yandex.ru>
2026-03-21 18:56:55 +0300

    Убрана зависимость от Winsock, пришлось перевести на C++23

--- Headers.hpp
+++ Headers.hpp
@@ -3,3 +3,4 @@
 struct ListNode* load(std::string path);
 bool write(struct ListNode* first, std::string path);
 void cleanup(struct ListNode* node);
+uint32_t to_little_endian(const uint32_t x);
--- Writer.cpp
+++ Writer.cpp
@@ -3,12 +3,20 @@
 #include <iostream>
 #include <cstdint>
 #include <array>
+#include <bit>
 #include <unordered_map>
-#include <winsock.h>
 #include "Headers.hpp"
 
+uint32_t to_little_endian(const uint32_t x) {
+	if (std::endian::native == std::endian::little) {
+		return x;
+	}
+	else {
+		return std::byteswap(x);
+	};
+};
 bool write(struct ListNode* first, std::string path) {
-	static const uint32_t magic = htonl(1337); // TODO перенести преобразование в какой-то один порядок байтов, но через функцию не из сетевого стека
+	static const uint32_t magic = to_little_endian(1337); // TODO перенести преобразование в какой-то один порядок байтов, но через функцию не из сетевого стека
 	std::unordered_map<ListNode*, int> indices{};
 	struct ListNode* current = first;
 	uint32_t i = 0;
@@ -17,7 +25,7 @@ bool write(struct ListNode* first, std::string path) {
 		current = current->next;
 		i++;
 	};
-	i = htonl(i);
+	i = to_little_endian(i);
 	std::ofstream f_stream{ path, std::ios::binary | std::ios::trunc };
 	if (!f_stream.good()) {
 		std::cerr << "Cannot open file" << std::endl;
@@ -30,9 +38,9 @@ bool write(struct ListNode* first, std::string path) {
 		current = first;
 		while (current) {
 			static const std::array<char, 8> zero_pad{};
-			const int32_t length = htonl(current->data.length());
+			const int32_t length = to_little_endian(current->data.length());
 			f_stream.write((const char*)&length, sizeof length); // Записываем длину пользовательских данных
-			const int32_t index = htonl(current->rand ? indices[current->rand] : -1);
+			const int32_t index = to_little_endian(current->rand ? indices[current->rand] : -1);
 			f_stream.write((const char*)&index, sizeof index); // Записываем индекс элемента, куда указывает rand
 			f_stream.write((const char*)current->data.data(), current->data.length()); // Записываем данные
 			f_stream.write(zero_pad.data(), (8 - (current->data.length() % 8)) & 7); // Записываем набивку до кратности в 8 байтов
